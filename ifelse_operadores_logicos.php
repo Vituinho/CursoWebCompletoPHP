3f1c2dbe7128906fd8402ad0664d248211e929ad
Vituinho <trabalhovictor531@gmail.com>
2025-08-11 09:25:54 -0300

    If Else Operadores Lógicos

--- ifelse_operadores_logicos.php
+++ ifelse_operadores_logicos.php
@@ -1,13 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Curso PHP</title>
 </head>
 <body>
-    <?php 
-        echo 'teste'
+    <a href="ifelse_praticando_mais_um_pouco.php">Proxima aula</a>
+    <?php
+     
+    //operadores condicionais/comparação
+    //==
+    //===
+    //!= ou <>
+    //!==
+    //<
+    //>
+    //<=
+    //>=
+
+    //operadores lógicos
+    //E: && ou AND -> retorna verdadeiro se todos os resultados forem verdadeiros
+    //OU: || ou OR -> retorna verdadeiro se pelo menos um dos retornar verdadeiro
+    //XOR: XOR -> retorna verdadeiro se uma das expressões for verdadeira e outra falsa ou vice-versa
+    //!
+
+    //() estabelecer precedência
+    //f e v = f
+    if((22 == 22 && 3 == 3) || 5 != 5) {
+        echo 'Verdadeiro';
+    } else {
+        echo 'Falso';
+    }
+
     ?>
 </body>
 </html>
---
+++ ifelse_praticando_mais_um_pouco.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <?php
+
+    //
+    $usuario_possui_cartao_loja = false;
+    $valor_compra = 225;
+
+    $valor_frete = 50;
+    $recebeu_desconto_frete = false;
+
+    if ($usuario_possui_cartao_loja == true && $valor_compra >= 100) {
+        $valor_frete = 0;
+        $recebeu_desconto_frete = true;
+    }
+    ?>
+
+    <h1>Detalhes do pedido</h1>
+    <p>Possui cartão da loja?
+        <?php
+        if ($usuario_possui_cartao_loja == true) {
+            echo 'SIM';
+        } else {
+            echo 'NÃO';
+        }
+        ?>
+    </p>
+    <p>Valor da compra: <?= $valor_compra ?></p>
+
+    <p>Recebeu desconto no frete?
+        <?php
+        if ($recebeu_desconto_frete == true) {
+            echo 'SIM';
+        } else {
+            echo 'NÃO';
+        }
+        ?>
+    </p>
+
+    <p>Valor do frete: <?= $valor_frete ?></p>
+</body>
+
+</html>
