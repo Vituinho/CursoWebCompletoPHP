9bcc27e985b8e9d7c2ef7929386e878d501e6453
Vituinho <trabalhovictor531@gmail.com>
2025-08-15 09:17:35 -0300

    App Help Desk - Autenticando usuário

--- app_help_desk/index.php
+++ app_help_desk/index.php
@@ -1,3 +1,11 @@
+<?php
+
+  if(isset($_GET['login'])) {
+  }
+  
+?>
+
+
 <html>
   <head>
     <meta charset="utf-8" />
@@ -39,6 +47,19 @@
                 <div class="form-group">
                   <input name="senha" type="password" class="form-control" placeholder="Senha">
                 </div>
+
+                <?php
+
+                  if (isset($_GET['login']) && $_GET['login'] == 'erro') {
+
+                ?>
+
+                <div class="text-danger">
+                    Usuário ou senha invalidos
+                </div>
+
+                <?php } ?>
+
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
             </div>
--- app_help_desk/valida_login.php
+++ app_help_desk/valida_login.php
@@ -1,5 +1,33 @@
 <?php
 
+    //variavel que ve se tá autenticado
+    $usuario_autenticado = false;
+
+    //usuarios do sistema
+
+    $usuarios_app = [
+        ['email' => 'adm@teste.com.br', 'senha' => '123456'],
+        ['email' => 'user@teste.com.br', 'senha' => 'abcd'],
+    ];
+
+    /*
+    echo '<pre>';
+    print_r($usuarios_app);
+    echo '</pre>';
+    */
+
+    foreach($usuarios_app as $user) {
+
+        if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+            $usuario_autenticado = true;
+        }
+    }
+
+    if ($usuario_autenticado) {
+            echo 'Usuário autenticado';
+        } else {
+            header('Location: index.php?login=erro');
+        }
 
     /*
     print_r($_GET);
@@ -9,7 +37,7 @@
     echo $_GET['email'];
     echo '<br>';
     echo $_GET['senha'];
-    */
+    
 
     print_r($_POST);
 
@@ -18,4 +46,5 @@
     echo $_POST['email'];
     echo '<br>';
     echo $_POST['senha'];
+    */
 ?>
