bea3d24fbc62b523056d074737696928b9d3cf34
Vituinho <trabalhovictor531@gmail.com>
2025-08-18 09:21:14 -0300

    App Help Desk - Registrando chamados

--- app_help_desk/registra_chamado.php
+++ app_help_desk/registra_chamado.php
@@ -1,17 +1,23 @@
 <?php
-    echo '<pre>';
-    print_r($_POST);
-    echo '</pre>';
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
 
-    fopen('arquivo.hd', 'a');
+//estamos trabalhando na montagem do texto
+$titulo = str_replace('#', '-', $_POST['titulo']);
+$categoria = str_replace('#', '-', $_POST['categoria']);
+$descricao = str_replace('#', '-', $_POST['descricao']);
 
-    $titulo = str_replace('#', '-', $_POST['titulo']);
-    $categoria = str_replace('#', '-', $_POST['categoria']);
-    $descricao = str_replace('#', '-', $_POST['descricao']);
+$texto = $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
 
-    //implode('#', $_POST);
+//abrindo o arquivo
+$arquivo = fopen('arquivo.hd', 'a');
+//escrevendo o texto
+fwrite($arquivo, $texto);
+//fechando o arquivo
+fclose($arquivo);
 
-    $texto = $_POST['titulo'] . '#' .  $_POST['categoria'] . '#' . $_POST['descricao'];
-    echo $texto;
+//echo $texto;
+header('Location: abrir_chamado.php');
 
 ?>
